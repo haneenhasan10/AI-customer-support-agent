133772a182353d820f6516673b28074e95a9bc1b
haneenhasan10 <haneenalomri12@gmail.com>
2026-03-16 09:14:59 +0300

    -

--- agent.py
+++ agent.py
@@ -1,30 +1,50 @@
+import streamlit as st
 from langchain_community.llms import Ollama
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_community.chat_message_histories import StreamlitChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
 
+# user interface 
+st.set_page_config(page_title=('AI support agent'))
+st.title('AI support agent')
 
+# use StreamlitChatMessageHistory to store current conversation 
+msgs = StreamlitChatMessageHistory(key = 'special_app_key')
+
+# load the model  
 llm = Ollama(model = 'llama3')
-knowledge = open("knowledge.txt").read()
 
-def ask_agent(question):
+with open("knowledge.txt", "r") as f:
+    knowledge_base = f.read()
 
-    prompt = f"""
-    You are a customer support AI agent.
-    Only answer using the information in the knowledge base.
-    If the information is not available, say you don't know..
-    Use this information to answer the question.
+# builed the prompt 
+prompt = ChatPromptTemplate.from_messages([
+    ("system", f"You are a professional customer support assistant.dont take side conversation,  Answer only based on the following information: \n{knowledge_base}"),
+    MessagesPlaceholder(variable_name="history"),
+    ("human", "{question}"),
+])
 
-    Knowledge:
-    {knowledge}
+# link the model with  memory and  prompt
+chain = prompt | llm
+chain_with_history = RunnableWithMessageHistory(
+    chain,
+    lambda session_id: msgs,
+    input_messages_key="question",
+    history_messages_key="history",
+)
 
-    Question:
-    {question}
-    """
+# display previous messages on the screen
+for msg in msgs.messages:
+    st.chat_message(msg.type).write(msg.content)
 
-    response = llm.invoke(prompt)
-
-    return response
-
-
-while True:
-    q = input("Ask a question: ")
-    answer = ask_agent(q)
-    print("Agent:", answer)
+# take user's input and process it
+if question := st.chat_input("How can I help you"):
+    st.chat_message("human").write(question)
+    
+    with st.spinner('Thinking'):
+        # send the question to the model
+        response = chain_with_history.invoke(
+            {"question": question},
+            config={"configurable": {"session_id": "any"}}
+        )
+        st.chat_message("ai").write(response)
